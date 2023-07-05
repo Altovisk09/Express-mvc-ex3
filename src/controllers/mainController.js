a42a4f0ae309e2f7a35afd7eca5b4bf8f35bdf3a
Altovisk <ericaltao13@gmail.com>
2023-07-04 22:06:08 -0300

    CRUD funcional

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -6,12 +6,24 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
+let visitado = products.filter(product => product.category == "visited")
+let oferta = products.filter(product => product.category == "in-sale")
+
 const controller = {
 	index: (req, res) => {
-		// Do the magic
+	 res.render('index', {
+		visitado,
+		oferta,
+		toThousand,
+	 })
 	},
 	search: (req, res) => {
-		// Do the magic
+		let search = req.query.keywords
+		let result = products.filter(product => product.name.toLowerCase().includes(search)) 
+		res.render('results', {
+			product: result,
+			toThousand,
+		})
 	},
 };
 
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,44 +1,86 @@
+const { json } = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const db = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(db, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
-	// Root - Show all products
+	// Exibe todos os produtos
 	index: (req, res) => {
-		// Do the magic
+		let product = products;
+		res.render('products', {
+			product,
+			toThousand
+		})
 	},
 
-	// Detail - Detail from one product
+	// Detalhes de um produto especifico
 	detail: (req, res) => {
-		// Do the magic
+		let id = req.params.id;
+		let product = products.find(product => product.id == id);
+		res.render('detail', {
+			product,
+			toThousand
+		})
 	},
 
-	// Create - Form to create
+	// view criar novo produto
 	create: (req, res) => {
-		// Do the magic
+		res.render('product-create-form')
 	},
 	
-	// Create -  Method to store
+	// Salva novo produto
 	store: (req, res) => {
-		// Do the magic
+		let newProduct = {
+			id: products[products.length - 1].id + 1,
+			...req.body,
+			image: 'default-image.png',
+		}
+		products.push(newProduct)
+		fs.writeFileSync(db, JSON.stringify(products, null, ' '));
+		res.redirect('/');
 	},
 
-	// Update - Form to edit
+	// Atualizar - view 
 	edit: (req, res) => {
-		// Do the magic
+		let id = req.params.id;
+		let product = products.find(product => product.id == id);
+		console.log(product)
+		res.render('product-edit-form', {
+			product
+		})
 	},
-	// Update - Method to update
+	// Atualizar - processo
 	update: (req, res) => {
-		// Do the magic
+		let id = req.params.id;
+		let productUpdate = products.find(product => product.id == id);
+
+		productUpdate = {
+			id: productUpdate.id,
+			...req.body,
+			image: productUpdate.image,
+		}
+
+		let modificationDb = products.map(product=>{
+			if(product.id == productUpdate.id){
+				return product = {...productUpdate}
+			}
+			return product
+		})
+
+		fs.writeFileSync(db , JSON.stringify(modificationDb, null, " "));
+		res.send('<script>alert("Produto atualizado"); window.location.href = "/"</script>');
 	},
 
-	// Delete - Delete one product from DB
+	// Deletar - Deleta um produto do banco de dados
 	destroy : (req, res) => {
-		// Do the magic
+		let id = req.params.id;
+		let finalProducts = products.filter(product => product.id != id);
+		fs.writeFileSync(db, JSON.stringify(finalProducts, null, ' '));
+		res.redirect('/');
 	}
 };
 
